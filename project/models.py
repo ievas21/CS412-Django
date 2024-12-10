1852370d352aef7f10e3a2b59d0d8381ffca6473
ievas21 <ievas@bu.edu>
2024-12-10 00:24:24 -0500

    Fixed styling issues

--- project/models.py
+++ project/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     Store/represent the data from one book.
     Title, Author, Score, Ratings, Shelvings, Published, Description, Image
     '''
-
+    # data attributes
     title = models.TextField()
     author = models.TextField()
     score = models.FloatField()
@@ -20,89 +20,97 @@ class Book(models.Model):
     image_url = models.URLField()
 
     def __str__(self):
-        '''Return a string representation of this model instance.'''
+        '''Return a string representation of the book instance.'''
         return f'{self.title} by {self.author}'
     
     def get_reviews(self):
         '''Retrieve all reviews for this Book'''
-
+        # use the ORM to filter reviews made for the specific book and order by timestamp
         reviews = Review.objects.filter(book=self).order_by('-timestamp')
-    
         return reviews
     
-    
-    
-class Person(models.Model):
-    '''Encapsulate the data for a Profile in our application'''
 
+class Person(models.Model):
+    '''
+    Encapsulate the data for a Person in our application.
+    '''
     # data attributes
     first_name = models.TextField(blank=False)
     last_name =  models.TextField(blank=False)
     email =  models.TextField(blank=False)
     image_url = models.TextField(blank=False) 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # associate each person with a user (one-to-one relationship)
+    # associate with a foreign key to a book, to represent a book a user is currently reading
     currently_reading = models.ForeignKey("Book", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        '''Return a string representation of this Profile'''
+        '''Return a string representation of this Person.'''
         return f"{self.first_name} {self.last_name}"
     
     def get_reviews(self):
-        '''Retrieve all reviews for this Profile'''
-
-    # use the ORM to filter Comments where this instance of an object is the FK
-        reviews = Review.objects.filter(person=self).order_by('-timestamp')
+        '''Retrieve all reviews that this Person instance made.'''
+        # use the ORM to filter reviews where this person instance is the FK
+        reviews = Review.objects.filter(person=self).order_by('-timestamp') # order by timestamp (most recent reviews show up first)
         return reviews
     
     def get_friends(self):
-        '''Retrieve all friendships for this Profile'''
-
+        '''Retrieve all friendships for this Person instance.'''
+        # filter the friend objects where they contain a FK to this person
         friends1 = Friend.objects.filter(profile1=self)
         friends2 = Friend.objects.filter(profile2=self)
     
-        # set of friend ids for the Profile
+        # empty set of friend ids for the person (friendship relationship must go both ways)
         friend_pks = set()  
         for f in friends1:
+            # add the friend's pk to the set
             friend_pks.add(f.profile2.pk) 
 
         for f in friends2:
+            # add the friend's pk to the set 
             friend_pks.add(f.profile1.pk)
 
+        # get the person objects associated with the friends and exclude the current logged-in person 
         friends = Person.objects.filter(pk__in=friend_pks).exclude(pk = self.pk)
         return list(friends)
     
     def get_reviews(self):
-        '''Retrieve all reviews that this user created.'''
+        '''Retrieve all reviews that this person created.'''
+         # use the ORM to filter reviews where this person instance is the FK
         reviews = Review.objects.filter(person=self).order_by('-timestamp')
         return reviews
     
     def get_book_list(self):
         '''Retrieve all books that this user has in their to-read list.'''
+        # use the ORM to filter the BookList object instance where this person is the FK
         books = BookList.objects.filter(person=self)
         return books
     
     def get_feed(self):
-        '''Gets all reviews for the profile and the profile's friends'''
+        '''Gets all reviews for the person and the person's friends'''
         friend_pks = set()
 
+          # filter the friend objects where they contain a FK to this person
         friend1 = Friend.objects.filter(profile1=self)
         for friend in friend1:
+             # add the friend's pk to the set 
             friend_pks.add(friend.profile2.pk)
                       
         friend2 = Friend.objects.filter(profile2=self)
         for friend in friend2:
+             # add the friend's pk to the set 
             friend_pks.add(friend.profile1.pk)
 
-        # add the profile itself, in order to display its messages as well
+        # add the profile itself, in order to display their reviews as well
         friend_pks.add(self.pk)
 
-        # get the messages associated with the friend profiles and order by their timestamp 
+        # get the reviews associated with the friend profiles and order by their timestamp 
         feed = Review.objects.filter(person_id__in=friend_pks).order_by('-timestamp')
         return feed
     
     def add_friend(self, other):
         # check that we're not making a friend relationship for a Profile with itself
         if (self != other):
+            # assign the two 
             friend1 = Friend.objects.filter(profile1=self, profile2=other)
             friend2 = Friend.objects.filter(profile1=other, profile2=self)
             # check that the friendship does not yet exist
@@ -115,10 +123,12 @@ class Person(models.Model):
 
         friend1 = Friend.objects.filter(profile1=self)
         for friend in friend1:
+             # add the friend's pk to the set 
             pks.add(friend.profile2.pk)
                       
         friend2 = Friend.objects.filter(profile2=self)
         for friend in friend2:
+             # add the friend's pk to the set 
             pks.add(friend.profile1.pk)
         
         suggestions = Person.objects.exclude(pk=self.pk).exclude(pk__in=pks)
--- project/urls.py
+++ project/urls.py
@@ -5,23 +5,23 @@ from django.contrib.auth import views as auth_views
 # Ieva sagaitis, ievas@bu.edu
 
 urlpatterns = [
-    path('', views.BooksListView.as_view(), name = "home"),
-    path('books', views.BooksListView.as_view(), name = "books"),
-    path('books/<int:pk>', views.BookDetailView.as_view(), name = "book_detail"),
-    path('users', views.UserListView.as_view(), name = "users"),
-    path('user/<int:pk>', views.UserDetailView.as_view(), name = "user_detail"),
-    path('book_reviews/<int:pk>', views.BookReviewDetailView.as_view(), name = "book_reviews"),
+    path('', views.BooksListView.as_view(), name = "home"), # shows all the books (serves as the main page of the application)
+    path('books', views.BooksListView.as_view(), name = "books"), # shows all the books (serves as the main page of the application)
+    path('books/<int:pk>', views.BookDetailView.as_view(), name = "book_detail"), # shows the details on one specific book that the user chose
+    path('users', views.UserListView.as_view(), name = "users"), # shows all the users in the application (can be accessed if not authenticated)
+    path('user/<int:pk>', views.UserDetailView.as_view(), name = "user_detail"), # shows the details of one specific user
+    path('book_reviews/<int:pk>', views.BookReviewDetailView.as_view(), name = "book_reviews"), # shows all reviews that pertain to one book
 
-    path(r'review/<int:pk>/update', views.UpdateReviewView.as_view(), name="update_review"),
-    path(r'review/<int:pk>/delete', views.DeleteReviewView.as_view(), name="delete_review"),
-    path(r'book/<int:book_pk>/review/create/', views.CreateReviewView.as_view(), name='create_review'),
-    path(r'books/', views.BooksListView.as_view(), name='books_list'),
-    path('statistics/', views.StatisticsListView.as_view(), name='statistics'),
-    path('feed/', views.ReviewListView.as_view(), name='feed'),
-    path(r'person/add_friends/<int:other_pk>', views.CreateFriendsView.as_view(), name="add_friends"),
-    path(r'profile/friend_suggest', views.ShowFriendSuggestView.as_view(), name="friend_suggest"), #
+    path(r'review/<int:pk>/update', views.UpdateReviewView.as_view(), name="update_review"), # shows the form used to update a review that a user created
+    path(r'review/<int:pk>/delete', views.DeleteReviewView.as_view(), name="delete_review"), # shows the form used to delete a review
+    path(r'book/<int:book_pk>/review/create/', views.CreateReviewView.as_view(), name='create_review'), # shows the form to create a review
+    path(r'books/', views.BooksListView.as_view(), name='books_list'), # shows the books as a list, another url
+    path('statistics/', views.StatisticsListView.as_view(), name='statistics'), # shows the statistics report for the books
+    path('feed/', views.ReviewListView.as_view(), name='feed'), # shows the logged-in user's feed
+    path(r'person/add_friends/<int:other_pk>', views.CreateFriendsView.as_view(), name="add_friends"), # redirects the user after adding a friend
+    path(r'profile/friend_suggest', views.ShowFriendSuggestView.as_view(), name="friend_suggest"), # shows the friend suggestions for a user
 
-    path('login/', auth_views.LoginView.as_view(template_name='project/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='project/logged_out.html'), name='logout'),
-     path(r'register/', views.CreateUserView.as_view(), name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='project/login.html'), name='login'), # shows the login page for users
+    path('logout/', auth_views.LogoutView.as_view(template_name='project/logged_out.html'), name='logout'), # shows the logout page for users
+     path(r'register/', views.CreateUserView.as_view(), name="register"), # redirects the user to the page to register an account and a profile associated with it
 ]
